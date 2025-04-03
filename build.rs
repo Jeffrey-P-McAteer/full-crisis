e1583379db4d35ee59796d296c142438edd638d5
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-04-02 21:04:48 -0400

    oops

--- build.rs
+++ build.rs
@@ -62,8 +62,6 @@ fn embed_icon() {
     println!("cargo::error=Error embedding icon in PE32+ exe file: {:?}", e);
   }
 
-  println!("cargo:rustc-link-arg-bins=resource.lib"); // This line, from https://github.com/mxre/winres/issues/32
-
 }
 
 
