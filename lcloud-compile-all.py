143b3a37f6ec7b29ea32c7f61ab631839029d033
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-06-15 10:29:24 -0400

    misc

--- lcloud-compile-all.py
+++ lcloud-compile-all.py
@@ -82,8 +82,18 @@ def get_ip_for_vm_hostname(vm_hostname):
 ####################
 
 def host():
+  print(f'[ host ] Running "host" stage on {socket.gethostname()}')
   setup_host_ip_space()
   user_at_host = f'{host_cloud_user}@{host_cloud_ip}'
+  # Copy project directory to cloud's /mnt/nfs/shared-vm-dir, which is shared to VMs
+  repo_dir = os.path.dirname(__file__).rstrip('/').rstrip('\\')
+  repo_dir_name = os.path.basename(repo_dir)
+  subprocess.run([
+    'rsync',
+      '-avz', '--progress', '-e', f'ssh -i "{host_cloud_key}"', '--exclude=target/docker-on-arch/',
+      f'{repo_dir}',
+      f'{user_at_host}:/mnt/nfs/shared-vm-dir/', # "/" at end will ensure /mnt/nfs/shared-vm-dir/full-crisis is created if not exists
+  ],check=True)
   # Copy self to cloud
   subprocess.run([
     'scp', '-i', host_cloud_key,
@@ -95,16 +105,49 @@ def host():
     'ssh', '-i', host_cloud_key,
       f'{user_at_host}', 'uv', 'run', f'/tmp/{SELF_FILE_NAME}', 'cloud'
   ],check=True)
+  # Copy built files back to local machine
+  print(f'[ host ] Copying built files back...')
+  subprocess.run([
+    'rsync',
+      '-avz', '--progress', '-e', f'ssh -i "{host_cloud_key}"',
+      f'{user_at_host}:/mnt/nfs/shared-vm-dir/{repo_dir_name}/.',
+      f'{repo_dir}',
+  ],check=True)
+  # Remove self just to be clean
+  subprocess.run([
+    'ssh', '-i', host_cloud_key,
+      f'{user_at_host}', 'rm', f'/tmp/{SELF_FILE_NAME}'
+  ],check=True)
+  print(f'[ host ] Done!')
 
 def cloud():
-  print(f'Running "cloud" stage on {socket.gethostname()}')
+  print(f'[ cloud ] Running "cloud" stage on {socket.gethostname()}')
+  # Spin up the external drive early and asyncronously
+  ignored_proc = subprocess.Popen(['ls', '/mnt/nfs'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+  win11_vm_ip = get_ip_for_vm_hostname('Builder-Win11')
+  if win11_vm_ip is not None:
+    # The windows 11 machine Z:\ drive is the same as the cloud's /mnt/nfs/shared-vm-dir, so we copy build material there
+    pass
+  else:
+    print(f'WARNING: Builder-Win11 is not running! Run with: virsh start Builder-Win11')
+  macos_vm_ip = get_ip_for_vm_hostname('Builder-MacOS')
+  if macos_vm_ip is not None:
+    pass
+  else:
+    print(f'WARNING: Builder-MacOS is not running! Run with: virsh start Builder-MacOS')
+  print(f'[ cloud ] Done!')
 
 
 def guest_win11():
-  pass
+  print(f'[ guest-win11 ] Running "guest-win11" stage on {socket.gethostname()}')
+
+  print(f'[ guest-win11 ] Done!')
 
 def guest_macos():
-  pass
+  print(f'[ guest-macos ] Running "guest-macos" stage on {socket.gethostname()}')
+
+  print(f'[ guest-macos ] Done!')
 
 
 # Call the stage function
