6cd162b6f77e407f074e1e189600dac577deab87
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-06-18 17:45:08 -0400

    forgot shutil

--- lcloud-compile-all.py
+++ lcloud-compile-all.py
@@ -14,6 +14,7 @@ import socket
 import time
 import threading
 import traceback
+import shutil
 
 paramiko = None
 try:
@@ -66,6 +67,10 @@ cloud_dhcp_lease_file = '/var/lib/libvirt/dnsmasq/virbr0.status'
 ####################
 windows_workdir = 'Z:\\full-crisis'
 macos_workdir = '/Volumes/nfs/shared-vm-dir/full-crisis'
+
+repo_dir = os.path.dirname(__file__).rstrip('/').rstrip('\\')
+ico_file = os.path.join(repo_dir, 'icon', 'full-crisis-icon.ico')
+png_file = os.path.join(repo_dir, 'icon', 'full-crisis-icon.png')
 
 
 ####################
@@ -148,6 +153,27 @@ def run_streaming_command(cmd):
     process.wait()
     return process.returncode
 
+
+def rreplace(s, old, new, occurrence=1):
+  li = s.rsplit(old, occurrence)
+  return new.join(li)
+
+
+def find_name_under(dir_name, file_name, max_recursion=8):
+  found_files = []
+  if max_recursion > 0 and os.path.exists(dir_name) and os.path.isdir(dir_name):
+    try:
+      for dirent in os.listdir(dir_name):
+        dirent_path = os.path.join(dir_name, dirent)
+        if dirent.casefold() == file_name.casefold():
+          found_files.append( dirent_path )
+        if os.path.isdir(dirent_path):
+          found_files += find_name_under(dirent_path, file_name, max_recursion=max_recursion-1)
+    except PermissionError:
+      print(f'Skipping {dir_name} because PermissionError')
+
+  return found_files
+
 ####################
 # Stage Logic
 ####################
@@ -183,8 +209,8 @@ def host():
   subprocess.run([
     'rsync',
       '-az', '--info=progress2', '-e', f'ssh -i "{host_cloud_key}"', '--exclude=.git/',
-      f'{user_at_host}:/mnt/nfs/shared-vm-dir/{repo_dir_name}/target/.',
-      f'{repo_dir}/target',
+      f'{user_at_host}:/mnt/nfs/shared-vm-dir/{repo_dir_name}/.',
+      f'{repo_dir}',
   ],check=True,bufsize=1,text=True)
   # Remove self just to be clean
   subprocess.run([
@@ -255,6 +281,7 @@ def cloud():
 
 def guest_win11():
   print(f'[ guest-win11 ] Running "guest-win11" stage on {socket.gethostname()}', flush=True)
+  # Step 1: Compile all .exe binaries
   for target in ['x86_64-pc-windows-gnu', 'x86_64-pc-windows-msvc', ]: # 'i686-pc-windows-gnu', 'i686-pc-windows-msvc']:
     subprocess.run([
       'rustup', 'target', 'add', f'{target}'
@@ -265,6 +292,50 @@ def guest_win11():
     subprocess.run([
       'cargo', 'build', '--release', f'--target={target}'
     ], cwd=windows_workdir, check=True)
+
+  # Step 2: Add in icons!
+  resource_hacker_folders = find_name_under(r'C:\Program Files', 'Resource Hacker', max_recursion=1)
+  resource_hacker_folders += find_name_under(r'C:\Program Files (x86)', 'Resource Hacker', max_recursion=1)
+
+  # This allows an "Anywhere" install to be picked up from a configured PATH env var
+  path_rh = shutil.which('ResourceHacker.exe')
+  if not (path_rh is None) and os.path.exists(path_rh):
+    resource_hacker_folders = [ os.path.dirname(path_rh) ] + resource_hacker_folders
+
+  if len(resource_hacker_folders) > 0:
+    resource_hacker_exes = find_name_under(resource_hacker_folders[0], 'ResourceHacker.exe')
+    if len(resource_hacker_exes) > 0:
+      resource_hacker_exe = resource_hacker_exes[0]
+      print(f'[ guest-win11 ] Found Resource Hacker at {resource_hacker_exe}')
+
+      full_crisis_exes = find_name_under(os.path.join(repo_dir, 'target', 'x86_64-pc-windows-gnu'), 'full-crisis.exe', max_recursion=2)
+      full_crisis_exes += find_name_under(os.path.join(repo_dir, 'target', 'x86_64-pc-windows-msvc'), 'full-crisis.exe', max_recursion=2)
+      full_crisis_exes = list(set(full_crisis_exes))
+
+      for full_crisis_exe in full_crisis_exes:
+        full_crisis_exe_with_icon = full_crisis_exe+'.icon.exe'
+        subprocess.run([
+          resource_hacker_exe,
+            '-open', full_crisis_exe,
+            '-save', full_crisis_exe_with_icon,
+            '-action', 'addskip',
+            '-res', ico_file,
+            '-mask', 'ICONGROUP,MAINICON,'
+        ], check=True)
+
+        if os.path.exists(full_crisis_exe_with_icon):
+          os.remove(full_crisis_exe)
+          shutil.copyfile(full_crisis_exe_with_icon, full_crisis_exe)
+          os.remove(full_crisis_exe_with_icon)
+          print(f'Added icon {ico_file} to {full_crisis_exe}')
+        else:
+          print(f'WARNING: {full_crisis_exe_with_icon} does not exist!')
+
+    else:
+      print(f'[ guest-win11 ] resource_hacker_exes = {resource_hacker_exes}')
+  else:
+    print(f'[ guest-win11 ] resource_hacker_folders = {resource_hacker_folders}')
+
   print(f'[ guest-win11 ] Done!', flush=True)
 
 def guest_macos():
