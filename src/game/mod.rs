3d5efe9a35e4d12d24c3c97896681d77d37761c8
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-06-20 12:41:00 -0400

    game state model ideas

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,12 +1,32 @@
 
 pub struct GameState {
-
+  pub active_event_loop: ActiveEventLoop,
 }
 
 impl GameState {
   pub fn new() -> Self {
     Self {
-
+      active_event_loop: ActiveEventLoop::WelcomeScreen(WelcomeScreen_View::Empty)
     }
   }
 }
+
+/// This tracks what event loop should be running
+pub enum ActiveEventLoop {
+  WelcomeScreen(WelcomeScreen_View),
+  ActiveGame(Game_View),
+}
+
+pub enum WelcomeScreen_View {
+  Empty,
+  NewGame,
+  LoadGame,
+  Settings
+}
+
+pub enum Game_View {
+
+
+}
+
+
