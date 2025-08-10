33b158a961e8ed3fab4fde0fb283fad7680ca167
Azure-Sidekick Build Daemon <jeffrey.p.mcateer@gmail.com>
2025-08-10 12:39:56 -0400

    Automated update from update-github-pages.py

---
+++ wasm32/full_crisis_web_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_5: WebAssembly.Table;
+export const closure658_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3ba1e98bcd16bcb9: (a: number, b: number) => void;
+export const closure660_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure3105_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
